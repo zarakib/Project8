e8a81848e8a49ccfce5e1dcbbcf4f9eec4aee18c
Rakib <zarakib70@gmail.com>
2019-09-28 23:45:03 +0600

    Project8

---
+++ CustomerOrderP/Home.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace CustomerOrderP
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.totalPriceTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.showDataGridView = new System.Windows.Forms.DataGridView();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(59, 37);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(62, 181);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "ADD";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(209, 30);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(109, 20);
+            this.nameTextBox.TabIndex = 2;
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(59, 100);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(18, 13);
+            this.idLabel.TabIndex = 3;
+            this.idLabel.Text = "ID";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(59, 71);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(45, 13);
+            this.addressLabel.TabIndex = 4;
+            this.addressLabel.Text = "Address";
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Location = new System.Drawing.Point(59, 140);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(58, 13);
+            this.totalPriceLabel.TabIndex = 5;
+            this.totalPriceLabel.Text = "Total Price";
+            // 
+            // totalPriceTextBox
+            // 
+            this.totalPriceTextBox.Location = new System.Drawing.Point(209, 140);
+            this.totalPriceTextBox.Name = "totalPriceTextBox";
+            this.totalPriceTextBox.Size = new System.Drawing.Size(109, 20);
+            this.totalPriceTextBox.TabIndex = 6;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(209, 100);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(109, 20);
+            this.idTextBox.TabIndex = 7;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(209, 68);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(109, 20);
+            this.addressTextBox.TabIndex = 8;
+            // 
+            // showDataGridView
+            // 
+            this.showDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.showDataGridView.Location = new System.Drawing.Point(51, 265);
+            this.showDataGridView.Name = "showDataGridView";
+            this.showDataGridView.Size = new System.Drawing.Size(524, 134);
+            this.showDataGridView.TabIndex = 9;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(486, 179);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 25);
+            this.updateButton.TabIndex = 10;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(391, 181);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(290, 181);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 12;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(178, 181);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 13;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.showDataGridView);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.totalPriceTextBox);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Home";
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.Label totalPriceLabel;
+        private System.Windows.Forms.TextBox totalPriceTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.DataGridView showDataGridView;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
---
+++ CustomerOrderP/Home.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerOrderP
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //connection 
+            string connectionString = @"Server=DESKTOP-IN2FMRP; Database=CoffeeShop2; Integrated Security= True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //command
+            string commanadString = @"INSERT INTO Customer (Name, Addres ) Values ('" + nameTextBox.Text + "', '" +addressTextBox.Text+ "')";
+            SqlCommand sqlCommand = new SqlCommand(commanadString, sqlConnection);
+
+
+            sqlConnection.Open();
+
+            sqlCommand.ExecuteNonQuery();
+
+            sqlConnection.Close();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            //connection 
+            string connectionString = @"Server=DESKTOP-IN2FMRP; Database=CoffeeShop2; Integrated Security= True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //command
+            string commanadString = @"SELECT * FROM Customer";
+            SqlCommand sqlCommand = new SqlCommand(commanadString, sqlConnection);
+
+
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+
+            showDataGridView.DataSource = dataTable;
+                sqlConnection.Close();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //connection 
+            string connectionString = @"Server=DESKTOP-IN2FMRP; Database=CoffeeShop2; Integrated Security= True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //command
+            string commanadString = @"SELECT * FROM Customer WHERE ID='" + idTextBox.Text + "'";
+            SqlCommand sqlCommand = new SqlCommand(commanadString, sqlConnection);
+
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            showDataGridView.DataSource = dataTable;
+
+            sqlConnection.Close();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            //connection 
+            string connectionString = @"Server=DESKTOP-IN2FMRP; Database=CoffeeShop2; Integrated Security= True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //command
+            string commanadString = @"DELETE FROM Customer WHERE ID = " + idTextBox.Text + "";
+            SqlCommand sqlCommand = new SqlCommand(commanadString, sqlConnection);
+
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            showDataGridView.DataSource = dataTable;
+
+            sqlConnection.Close();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            //connection 
+            string connectionString = @"Server=DESKTOP-IN2FMRP; Database=CoffeeShop2; Integrated Security= True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+            //command
+            string commanadString = @"UPDATE Customer SET Addres =" + addressTextBox.Text + " WHERE ID = " + idTextBox.Text + "";
+            SqlCommand sqlCommand = new SqlCommand(commanadString, sqlConnection);
+
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            showDataGridView.DataSource = dataTable;
+
+            sqlConnection.Close();
+        }
+    }
+}
